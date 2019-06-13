3df3994c907f61edf90eafb8367feafca8ab7761
Candice Parker <lexus2424@gmail.com>
2019-06-13 20:03:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(avi_attributes)
-   avi_atrributes.each do |key, value|
+   avi_attributes.each do |key, value|
      self.send("#{key}=", value)
   end
 end
