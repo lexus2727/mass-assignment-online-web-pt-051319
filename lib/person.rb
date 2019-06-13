94e68043d7eb66e7960554a1b2be78f3a14f4b04
Candice Parker <lexus2424@gmail.com>
2019-06-13 20:04:26 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,31 @@
 class Person
-  #your code here
+  
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
+  def initialize(avi_attributes)
+   avi_attributes.each do |key, value|
+     self.send("#{key}=", value)
+  end
 end
+  
+  
+  
+  
+  
+  
+end
+
+
+# :name => "Avi",
+ #     :birthday => "01/29/1984",
+ #     :hair_color => "brown",
+  #    :eye_color => "brown",
+    #  :height => "short",
+     # :weight => "good",
+     # :handed => "lefty",
+     # :complexion => "decent",
+     # :t_shirt_size => "medium",
+     # :wrist_size => "small",
+     # :glove_size => "normal",
+      #:pant_length => "32",
+      #:pant_width => "32"
