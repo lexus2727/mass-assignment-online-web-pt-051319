81193c6bf1396d9b952da66de869e80b13909627
Candice Parker <lexus2424@gmail.com>
2019-06-13 19:50:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,8 @@
 class Person
   
-  atrr
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handy, :complexion, :t_shirt_size, :waist_size, :glove_size, :pant_length, :pant_width
+  
+  def initialize()
   
   
   
@@ -8,3 +10,16 @@ class Person
 end
 
 
+# :name => "Avi",
+ #     :birthday => "01/29/1984",
+ #     :hair_color => "brown",
+  #    :eye_color => "brown",
+    #  :height => "short",
+     # :weight => "good",
+     # :handed => "lefty",
+     # :complexion => "decent",
+     # :t_shirt_size => "medium",
+     # :wrist_size => "small",
+     # :glove_size => "normal",
+      #:pant_length => "32",
+      #:pant_width => "32"
